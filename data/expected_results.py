7fd114a2edf6b807fd4a23d8b4bf9988b1293c1b
amielpeled <amielnoy@gmail.com>
2025-03-10 18:48:48 +0200

    create expected titles

--- data/expected_results.py
+++ data/expected_results.py
@@ -1,7 +1,9 @@
 class ExpectedResults:
         EXPECTED_BOOK_NUMBER = 2
         EXPECTED_BOOK_NUMBER_AFTER_ADD =3
-        EXPECTED_BOOK_TITLE = "1984"
+        EXPECTED_BOOK_TITLE1 = "The Great Gatsby"
+        EXPECTED_BOOK_TITLE2 = "To Kill a Mockingbird"
+
         EXPECTED_BOOK_AUTHOR = "George Orwell"
         EXPECTED_UPDATED_BOOK_TITLE = "The Great Gatsby Updated"
         EXPECTED_BOOK_BORROW_MESSAGE = "Book borrowed successfully"
